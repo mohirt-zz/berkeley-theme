6fedf91c774286f82a72e6fe68059360690f0de3
vagrant <vagrant@berkeleylocaldev.berkeley.dgicloud.com>
2013-08-07 17:45:24 +0000

    Started to integrate Berkeley's new brand elements, colours, links, h tags etc.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -71,7 +71,9 @@
 ?>
 
 <div id="head-back"></div>
+<div id="logo-back"></div>
 <div id="page">
+    
   <header id="header" role="banner">
 
     <?php if ($logo): ?>
@@ -107,32 +109,6 @@
         )); ?>
       </nav>
     <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-
-  </header>
-
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div><!-- /#content -->
-
     <div id="navigation">
 
       <?php if ($main_menu): ?>
@@ -159,6 +135,31 @@
       <?php print render($page['navigation']); ?>
 
     </div><!-- /#navigation -->
+   
+    <?php print render($page['header']); ?>
+
+  </header>
+
+  <div id="main">
+    
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div><!-- /#content -->
 
     <?php
       // Render the sidebars to see if there's anything in them.
